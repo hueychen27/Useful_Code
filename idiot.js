ec323bbf4a6a7b42f25e86ee8dfbfd5b44f3ce3c
Da_Person <hueychen27@outlook.com>
2023-06-22 14:01:30 -0700

    add files

---
+++ idiot.js
@@ -0,0 +1,13 @@
+class Idiot {
+    constructor (iq, brainCells) {
+        this.iq = iq;
+        this.brainCells = brainCells
+    }
+    speak() {
+        const brainpower = this.iq * this.brainCells
+        if (brainpower < 50) { console.log("i no brain cell"); }
+        if (brainpower >= 50) { console.warn("I nerd") }
+    }
+}
+const idiot = new Idiot(-1, 0)
+idiot.speak()
